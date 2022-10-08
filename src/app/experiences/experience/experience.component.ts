774cd61bc68bb44c9552dfee1edcbd72444ac6b5
Sanjay Garg <sgarg5858@gmail.com>
2022-10-08 13:46:36 +0530

    experiences refactoring done!

--- src/app/experiences/experience/experience.component.ts
+++ src/app/experiences/experience/experience.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Employer } from '../experience.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatCardModule],
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
 
+  @Input() employer!:Employer;
   constructor() { }
 
   ngOnInit(): void {
